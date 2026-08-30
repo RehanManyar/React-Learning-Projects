9980a711a892f852d6ad3ad5da6154864d9c3b0f
RehanManyar <rehan.manyar@outlook.in>
2026-08-30 09:56:13 +0530

    Initial commit

--- 03-folder/Bharat clock/src/Time.jsx
+++ 03-folder/Bharat clock/src/Time.jsx
@@ -1,7 +1,18 @@
-function Time(){
-    return (
-        <p>This is t current Time 30/08/2026 - 09:39 AM</p>
-    );
+
+function Time() {
+  const now = new Date();
+
+  const date = now.toLocaleDateString("en-GB");
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  return (
+    <p>
+      This is the current Time {date} - {time}
+    </p>
+  );
 }
 
 export default Time;
